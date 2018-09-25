f53661d68d99cb730414022ead5526ae9241a90b
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-09-25 18:29:12 -0400

    Completed vid 28

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -156,4 +156,88 @@ router.post(
   }
 );
 
+//@route POST api/posts/comment/:id
+//@desc Add a comment to post:id
+//@access Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Check validation
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      //if any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        //Add to comments array
+        post.comments.unshift(newComment);
+
+        //save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(404).json({ postnotfound: "No post found" });
+      });
+  }
+);
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc Remove a specific comment:comment_id from a post:id
+//@access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Check validation
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      //if any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        //check to see if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "That comment cannot be found" });
+        }
+
+        //Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //Splice it out of array
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(404).json({ postnotfound: "No post found" });
+      });
+  }
+);
+
 module.exports = router;
